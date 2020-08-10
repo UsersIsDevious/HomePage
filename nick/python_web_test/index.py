c27615e9dae026e10bedc329f40ba0b172bf9f74
Nick-Sugar <nicksugar0141@gmail.com>
2020-08-11 00:19:08 +0900

    test_6

--- nick/python_web_test/index.py
+++ nick/python_web_test/index.py
@@ -1,7 +1,10 @@
 from browser import document, alert
 
 def echo(event):
-	alert(document["zone"].value)
 	rslt = document["result"]
+	if document["zone"].value == "0505keitan":
+		alert('本当に'+document["zone"].value+'でいいですか')
+		rslt.text = "お前恐ろしいお文字列打つな.....怖"
+	alert(document["zone"].value+'が入力されています')
 	rslt.text = document["zone"].value
 document["mybutton"].bind("click", echo)
