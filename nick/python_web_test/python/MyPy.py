e0b302ddb1fbd2a5a8497224efb026c79bb793da
Nick-Sugar <nicksugar0141@gmail.com>
2020-08-10 21:18:00 +0900

    Pythonをテストしてみた

---
+++ nick/python_web_test/python/MyPy.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+from browser import document
+
+def Addition():
+    data_1 = float(document["data_1"].value)
+    data_2 = float(document["data_2"].value)
+
+    Results_Addition = str(data_1+data_2)
+    rslt = document["result_Addition"]
+    rslt.text = Results_Addition
+
+#ボタンの名前定義
+addition_btn = document["Addition"]
+#clickが発火したら呼ばれるって定義
+addition_btn.bind("click", Addition)
