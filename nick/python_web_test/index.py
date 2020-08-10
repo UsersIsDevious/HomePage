76af3f219a1fcca80c64d1488c3be7ba89806cfa
Nick-Sugar <nicksugar0141@gmail.com>
2020-08-11 01:46:28 +0900

    test_10

--- nick/python_web_test/index.py
+++ nick/python_web_test/index.py
@@ -1,10 +1,10 @@
-from browser import document, alert
+from browser import document, alert,html
 
 def echo(event):
 	rslt = document["result"]
 	if document["zone"].value == "0505keitan":
 		alert('本当に'+document["zone"].value+'でいいですか')
-		rslt.options.insert("<h1>お前恐ろしいお文字列打つな.....怖</h1>")
+		rslt <= html.H1("お前恐ろしいお文字列打つな.....怖")
 	else:
 		alert(document["zone"].value+'が入力されています')
 		rslt.text = document["zone"].value
