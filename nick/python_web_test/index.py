86250514cec002329bb55ce48bdc106f03329625
Nick-Sugar <nicksugar0141@gmail.com>
2020-08-11 01:34:11 +0900

    test_9

--- nick/python_web_test/index.py
+++ nick/python_web_test/index.py
@@ -4,7 +4,7 @@ def echo(event):
 	rslt = document["result"]
 	if document["zone"].value == "0505keitan":
 		alert('本当に'+document["zone"].value+'でいいですか')
-		rslt = "<h1>お前恐ろしいお文字列打つな.....怖</h1>"
+		rslt.options.insert("<h1>お前恐ろしいお文字列打つな.....怖</h1>")
 	else:
 		alert(document["zone"].value+'が入力されています')
 		rslt.text = document["zone"].value
