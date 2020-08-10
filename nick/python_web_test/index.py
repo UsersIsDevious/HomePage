88ed447732b37f961cd90f255dbeb1d10a5692e5
Nick-Sugar <nicksugar0141@gmail.com>
2020-08-10 22:16:05 +0900

    test_2

--- nick/python_web_test/index.py
+++ nick/python_web_test/index.py
@@ -1,35 +1,21 @@
-#!/usr/bin/python3
-# coding: utf-8
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>BrythonでBMIを計算する</title>
+        <meta charset="utf-8">
+        <script src="static/brython.js"></script>
+        <script src="static/brython_stdlib.js"></script>    
+    </head>
+    <body onload="brython()">
+　　　　 <script type="text/python" src="static/bmi.py"></script>
+        <h1>BrythonでBMIを計算する</h1>
 
-# import
-import cgitb,io,sys
+        <p>身長(メートル)
+        <input type="text" id="height" />
+        <p>体重(キログラム)
+        <input type="text" id="weight" />
+        <br><button id="execute">計算だ!</button>
 
-# エラー表示
-cgitb.enable()
-
-#日本語用
-sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
-
-#ヘッダー出力
-print ("Content-Type: text/html; charset=UTF-8;\n\n")
-
-# 置換えデータ作成（サンプル用）
-page_data = {}
-page_data['site_title'] = 'ONE NOTES'
-page_data['page_title'] = 'Pythonサンプルページ'
-page_data['header'] = '<h1>ONE NOTES</h1>'
-page_data['contents'] = '<h2>'+ page_data['page_title'] +'</h2><p>Pythonを使って作成したサンプルページです</p>'
-page_data['sidebar'] = '<p>サイドバー</p>'
-page_data['footer'] = '<p>フッター</p>'
-
-# template.htmlの読み込み
-with open('template.html','r') as file:
-    html = file.read()
-file.closed
-
-# {% %}をpage_dataに置換え
-for key, value in page_data.items():
-    html = html.replace('{% ' + key + ' %}', value)
-
-#HTML出力
-print(html)
+        <div id="result"></div>
+    </body>
+</html>
